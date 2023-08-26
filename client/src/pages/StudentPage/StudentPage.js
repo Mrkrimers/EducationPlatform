a61fd8c41dfed56699c398a8d55eea25713bcdf8
mrkrimers <mrkrimerssasha@gmail.com>
2023-08-26 23:39:47 +0300

    added

--- client/src/pages/StudentPage/StudentPage.js
+++ client/src/pages/StudentPage/StudentPage.js
@@ -3,18 +3,9 @@ import Header from "../../components/Header/Header";
 import style from "./style.module.css"
 
 function StudentPage() {
-    const arr = [{
-        h1: 'JavaScript',
-        p: 'JavaScript is a practical course where students learn the basics of JavaScript. It covers variables, operators, conditionals, loops, functions, and data manipulation.'
-    },
-    {
-        h1: 'TypeScript',
-        p: `TypeScript is a course that provides an introduction to TypeScript. Students will learn about TypeScript's key features, such as type annotations, interfaces, classes, and modules`
-    },
-    {
-        h1: 'Python',
-        p: `Students will learn about variables, data types, conditionals, loops, functions, and file handling. Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.`
-    }]
+    const arr = [{ h1: 'JavaScript', p: 'JavaScript is a practical course where students learn the basics of JavaScript. It covers variables, operators, conditionals, loops, functions, and data manipulation.' },
+    { h1: 'TypeScript', p: `TypeScript is a course that provides an introduction to TypeScript. Students will learn about TypeScript's key features, such as type annotations, interfaces, classes, and modules` },
+    { h1: 'Python', p: `Students will learn about variables, data types, conditionals, loops, functions, and file handling. Through hands-on exercises and projects, students will gain proficiency in writing Python code and solving real-world problems.` }]
 
     return (
         <>
